29589b149e92477c1b7112b93a8f2ce28070a64d
Andrey Bezpalov <abezpalov@ya.ru>
2018-01-29 13:14:08 +0400

    dataminer.exporters.liqui

---
+++ daven/dataminer/exporters/liqui.py
@@ -0,0 +1,134 @@
+import json
+import time
+import datetime
+from django.utils import timezone
+
+import numpy as np
+import pandas as pd
+
+from dataminer.models import *
+
+
+class Worker():
+
+    def __init__(self):
+        self.bourse = Bourse.objects.take(**{'name': 'liqui', 'full_name': 'Liqui.io',
+                                             'base_url': 'https://api.liqui.io/api/3'})
+        print('Bourse:', self.bourse)
+
+    def run(self):
+
+        start_time = timezone.now()
+
+        pairs = Pair.objects.filter(bourse = self.bourse)
+        for n, pair in enumerate(pairs):
+            df = self.pair_infos_to_df(pair)
+            date = np.max(df.created_at)
+            date = str(date).split(' ')[0]
+            file_name = '~/export/{} {}.csv.tar.gz'.format(date, str(pair))
+            df.to_csv(file_name, compression='gzip', index_label='idx')
+            print(n, file_name)
+
+
+
+        print('Цикл завершен.\nВремя завершения: {}\nВремя выполнения{}\n\n'.format(timezone.now(), timezone.now() - start_time))
+
+
+    def pair_infos_to_df(self, pair):
+
+        # Готовим словарь с массивами
+        keys = ['info_id', 'bourse_name', 'pair_id', 'pair_name',
+                 'ticker_high', 'ticker_low', 'ticker_avg', 'ticker_vol', 'ticker_vol_cur', 'ticker_last',
+                 'ticker_buy', 'ticker_sell', 'ticker_updated']
+        for i in range(2000):
+            keys.append('order_ask_{}_price'.format(i))
+            keys.append('order_ask_{}_vol'.format(i))
+        for i in range(2000):
+            keys.append('order_bid_{}_price'.format(i))
+            keys.append('order_bid_{}_vol'.format(i))
+        for i in range(2000):
+            keys.append('trade_{}_ask'.format(i))
+            keys.append('trade_{}_bid'.format(i))
+            keys.append('trade_{}_price'.format(i))
+            keys.append('trade_{}_amount'.format(i))
+            keys.append('trade_{}_tid'.format(i))
+            keys.append('trade_{}_timestamp'.format(i))
+        keys.append('created_at')
+
+        data = {}
+        for key in keys:
+            data[key] = []
+
+        infos = PairJSONInfo.objects.filter(pair = pair)
+        for info in infos:
+
+            # Base info
+            data['info_id'].append(str(info.id))
+            data['bourse_name'].append(str(pair.bourse.name))
+            data['pair_id'].append(str(pair.id))
+            data['pair_name'].append(str(pair.name))
+
+            # Ticker
+            ticker = json.loads(str(info.ticker).replace("'", '"'))
+            data['ticker_high'].append(ticker['high'])
+            data['ticker_low'].append(ticker['low'])
+            data['ticker_avg'].append(ticker['avg'])
+            data['ticker_vol'].append(ticker['vol'])
+            data['ticker_vol_cur'].append(ticker['vol_cur'])
+            data['ticker_last'].append(ticker['last'])
+            data['ticker_buy'].append(ticker['buy'])
+            data['ticker_sell'].append(ticker['sell'])
+            data['ticker_updated'].append(ticker['updated'])
+
+            # Orders
+            orders = json.loads(str(info.orders).replace("'", '"'))
+
+            i = 0
+            for order in orders['asks']:
+                price, vol = order
+                data['order_ask_{}_price'.format(i)].append(price)
+                data['order_ask_{}_vol'.format(i)].append(vol)
+                i += 1
+            while i < 2000:
+                data['order_ask_{}_price'.format(i)].append(None)
+                data['order_ask_{}_vol'.format(i)].append(None)
+                i += 1
+
+            i = 0
+            for order in orders['bids']:
+                price, vol = order
+                data['order_bid_{}_price'.format(i)].append(price)
+                data['order_bid_{}_vol'.format(i)].append(vol)
+                i += 1
+            while i < 2000:
+                data['order_bid_{}_price'.format(i)].append(None)
+                data['order_bid_{}_vol'.format(i)].append(None)
+                i += 1
+
+            # Trades
+            trades = json.loads(str(info.trades).replace("'", '"'))
+            i = 0
+            for trade in trades:
+                if trade['type'] == 'ask':
+                    data['trade_{}_ask'.format(i)].append(True)
+                    data['trade_{}_bid'.format(i)].append(False)
+                else:
+                    data['trade_{}_ask'.format(i)].append(False)
+                    data['trade_{}_bid'.format(i)].append(True)
+                data['trade_{}_price'.format(i)].append(trade['price'])
+                data['trade_{}_amount'.format(i)].append(trade['amount'])
+                data['trade_{}_tid'.format(i)].append(trade['tid'])
+                data['trade_{}_timestamp'.format(i)].append(trade['timestamp'])
+                i += 1
+            while i < 2000:
+                data['trade_{}_ask'.format(i)].append(False)
+                data['trade_{}_bid'.format(i)].append(False)
+                data['trade_{}_price'.format(i)].append(None)
+                data['trade_{}_amount'.format(i)].append(None)
+                data['trade_{}_tid'.format(i)].append(None)
+                data['trade_{}_timestamp'.format(i)].append(None)
+                i += 1
+
+            data['created_at'.format(i)].append(info.created_at)
+
+        return pd.DataFrame(data, columns = keys)
---
+++ daven/dataminer/import.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from decouple import config
+
+from django.utils import timezone
+
+SYS_PATH = config('SYS_PATH')
+sys.path.append(SYS_PATH)
+os.environ['DJANGO_SETTINGS_MODULE'] = 'daven.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
+# Выполняем необходимый загрузчик
+print("Пробую выполнить загрузчик " + sys.argv[1])
+Worker = __import__('dataminer.importers.' + sys.argv[1], fromlist=['Worker'])
+
+# Тестовый режим больше логов на экране
+for arg in sys.argv:
+    if '-t' == arg:
+        test = True
+        break
+    else:
+        test = False
+
+# Режим многопоточности
+for arg in sys.argv:
+    if '-m' == arg:
+        mp = True
+        break
+    else:
+        mp = False
+
+worker = Worker.Worker()
+worker.test = test
+worker.mp = mp
+worker.run()
+
+exit()
